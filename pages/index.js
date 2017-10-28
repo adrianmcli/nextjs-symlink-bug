75a35b38c9ff50e8cf965d42417f1cf55642b305
Adrian Li <li.adrianmc@gmail.com>
2017-10-28 02:53:10 -0700

    implement fix to bug

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 // If you use the symlink-folder line instead of the normal-folder line
 // and do a hard refresh, Next.js will break:
-import someObj from '../lib/normal-folder/someText'
-// import someObj from '../lib/symlink-folder/someText'
+// import someObj from '../lib/normal-folder/someText'
+import someObj from '../lib/symlink-folder/someText'
 
 export default () => <div>{someObj.text}</div>
